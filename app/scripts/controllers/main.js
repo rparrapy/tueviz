a2c909f07382ddb78ad2f92f188e2d362c1af4cd
Rodrigo Parra <rodpar07@gmail.com>
2016-01-27 20:23:29 +0100

    Fix layout bug for the barchart

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -39,7 +39,7 @@ angular.module('tueVizApp')
       }
     ];
 
-    $scope.menu.selected = $scope.menu.items[1];
+    $scope.menu.selected = $scope.menu.items[2];
     $scope.menu.selectEntry = function(e) {
       $scope.menu.selected = e;
       $location.path(e.fragment);
